942d9e2ea65d30c617bf5df1284ed349c6657366
tshehan6 <tshehan6@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-11-21 05:54:21 +0000

    Temporarily removed the actual html output, but made other improvements to fit the design specification

--- trunk/output.lisp
+++ trunk/output.lisp
@@ -1,150 +1,128 @@
-(in-package "ACL2")
-(include-book "io-utilities" :dir :teachpacks)
-(include-book "list-utilities" :dir :teachpacks)
+#|This line is 75 characters long.---------------------------------------|#
+#|tImpl: Stock History Analysis
+  Moutput.lisp
+  Outputs the data as an HTML file.
+  Date: December 6th, 2012
+  Team Dijkstra
+|#
 
+  (in-package "ACL2")
+  (include-book "io-utilities" :dir :teachpacks)
+  (include-book "list-utilities" :dir :teachpacks)
 
-
-
-; These four functions are required
-(defun sum (xs)
+  
+  ; private
+  (defun sum (xs)
   (if (consp (cdr xs)) 
       (+ (car xs) (sum (cdr xs)))
       (car xs)
   )
-)
-(defun avg (xs)
-  (/ (sum xs) (len xs))
-)
-(defun vector+scalar (xs x)
-  (if (consp (cdr xs))
-    (cons (+ x (car xs)) (vector+scalar (cdr xs) x)) 
-    (list (+ x (car xs)))
   )
-)
-(defun vector*vector (xs ys)
-  (if (consp (cdr xs))
-      (cons (* (car xs) (car ys)) (vector*vector (cdr xs) (cdr ys)) )
-      (list (* (car ys) (car xs)))
+  (defun avg (xs)
+    (/ (sum xs) (len xs))
   )
-)
-
-; list containing the differences between each 
-; single point and the average of a list
-(defun differences (xs)
-  (if (consp (cdr xs))
-    (cons (-(car xs) (avg xs)) (differences (cdr xs)))
-    (-(car xs) (avg xs))
-  )
-)
-
-; Variance
-(defun xVar (xs)
-  (let* ((ds (differences xs)))
-    (let* ((sds (vector*vector ds ds)))
-      (avg sds)
+  (defun vector+scalar (xs x)
+    (if (consp (cdr xs))
+      (cons (+ x (car xs)) (vector+scalar (cdr xs) x)) 
+      (list (+ x (car xs)))
     )
   )
-)
-
-; r(x,y)
-(defun r (xs ys)
-  (let* ((xds (differences xs)) (yds (differences ys)))
-    (let* ((xyds (vector*vector xds yds)))
-      (avg xyds)
+  (defun vector*vector (xs ys)
+    (if (consp (cdr xs))
+        (cons (* (car xs) (car ys)) (vector*vector (cdr xs) (cdr ys)) )
+        (list (* (car ys) (car xs)))
     )
   )
-)
 
-(defun getY (x a b)
-  (+(* a x) b)
-)
-
-(defun getYs (xs a b)
-  (if (consp (cdr xs))
-      (cons (getY(car xs) a b) (getYs (cdr xs) a b)) 
-      (list (getY (car xs) a b))
+  ; list containing the differences between each 
+  ; single point and the average of a list
+  (defun differences (xs)
+    (if (consp (cdr xs))
+      (cons (-(car xs) (avg xs)) (differences (cdr xs))) 
+      (-(car xs) (avg xs))
+    )
   )
-)
 
+  ; Variance
+  (defun xVar (xs)
+    (let* ((ds (differences xs)))
+      (let* ((sds (vector*vector ds ds)))
+        (avg sds)
+      )
+    )
+  )
 
-(defun combine (xs ys zs)
+  ; r(x,y)
+  (defun r (xs ys)
+    (let* ((xds (differences xs)) (yds (differences ys)))
+      (let* ((xyds (vector*vector xds yds)))
+        (avg xyds)
+      )
+    )
+  )
+  (defun getY (x a b)
+    (+(* a x) b)
+  )
+  (defun getYs (xs a b)
+    (if (consp (cdr xs))
+        (cons (getY(car xs) a b) (getYs (cdr xs) a b)) 
+        (list (getY (car xs) a b))
+    )
+  )
+  (defun combine (xs ys zs)
     (if (consp (cdr xs)) 
         (concatenate 'string (concatenate 'string "[" (rat->str(car xs) 2) "," (rat->str(car ys) 2) "," (rat->str (car zs) 2) "]" ) "," (combine (cdr xs) (cdr ys) (cdr zs))  ) 
         (concatenate 'string "[" (rat->str (car xs) 2) "," (rat->str (car ys) 2) "," (rat->str (car zs) 2) "]" ) 
     )
-)
+  )
+  (defun firsts (tups)
+    (if (consp (cdr tups))
+        (cons (first (car tups)) (firsts (cdr tups)))
+        (list (first (car tups)))
+    )
+  )
+  (defun seconds (tups)
+    (if (consp (cdr tups))
+        (cons (second (car tups)) (seconds (cdr tups)))
+        (list (second (car tups)))
+    )
+  )
 
-
-(defun regressionPoints (xs ys)
-  (let* ((xAvg (avg xs)) (yAvg (avg ys)))
-    (let* ((b (/ (r xs ys) (xVar xs))))
-      (let* ((a (- yAvg (* b xAvg))))
-       (combine xs ys (getYs xs b a))
+  
+  ; defuns
+  (defun generateData (dates sums)
+    ; do the linear regression to get a and b
+    ; then use algebra to get a list of regression values at every date (getYs)
+    ; then combine dates sums and regression values into a list of triples as a string
+    (let* ((xAvg (avg dates)) (yAvg (avg sums)))
+      (let* ((b (/ (r dates sums) (xVar dates))))
+        (let* ((a (- yAvg (* b xAvg))))
+         (combine dates sums (getYs dates b a))
+        )
       )
     )
   )
-)
 
+  (defun outputStockData (data)
+    (if (consp (cdr data)) 
 
+        ; Recursive Case
+        ; the below line doesn't really need these consed, it's just
+        ; a hack to do something to the car and then something to the
+        ; cdr. the values returned are irrelevant.
+        (cons (outputStockData (list(car data))) (outputStockData (cdr data))) 
 
-(defun modify-file (str)
-   (list (chrs->str (str->chrs str))))
-
-(defun getHTML (xs ys)
-  (concatenate 'string 
-               "
-<html>
-  <head>
-    <script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>
-    <script type=\"text/javascript\">
-      google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable(
-               "
-               
-               
-               "[" "['x','y','regression']," (regressionPoints xs ys) "]"
-
-               
-               "
-);
-        var options = {
-          title: \"Pi / 4 Using the Monte Carlo method\"
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById(\"chart_div\"));
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-  <body>
-    <div id=\"chart_div\" style=\"width: 900px; height: 500px;\"></div>
-  </body>
-</html>
-               "
+        ; Base Case - called once for every recursive case
+        ;first take the car, because we don't need the names
+        (let* ((values (car (car data))))
+          ;then change that into a list of dates and a list of sums (xs and ys)
+          (let* ((dates (firsts values)) (sums (seconds values)))
+            ;get the linear regression data (in JSON form)
+            (generateData dates sums) 
+          )
+        )
+    )
   )
-)
-
-(defun rtw (f-in f-out state)
-  (mv-let (input-as-string error-open state) (file->string f-in state)
-     (if error-open
-         (mv error-open state)
-         (mv-let (error-close state)
-                 (string-list->file f-out
-                                    (modify-file (getHTML '(1 2 3 4) '(5 6 4 5)))
-                                    state)
-            (if error-close
-                (mv error-close state)
-                (mv (string-append "input file: "
-                     (string-append f-in
-                      (string-append ", output file: " f-out)))
-                    state))))))
-
-
-
-
-
-
-;(getHTML '(1 2 3 4) '(5 5 5 5))
-(rtw "in.txt" "out.html" state)
+  
+  (outputStockData (list ( list (list '(1 5) '(2 10) '(3 5)) (list "GOOG" "YHOO")) ( list (list '(1 5) '(2 10) '(3 5)) (list "GOOG" "YHOO")))  )
+  
