535b4e27cb6bae08d4900d3f08e0ae763f58d2ac
matthew.a.crist@gmail.com <matthew.a.crist@gmail.com@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-11-27 19:37:17 +0000

    Added XML Parser data for Stock Records.

---
+++ InputModule_001.lisp
@@ -0,0 +1,47 @@
+(include-book "io-utilities" :dir :teachpacks)
+
+; (extractXMLTag xml tag)
+; Extracts the contents of an XML tags (all the data that is contained between
+; the tags)
+; xml - the string that contains the RAW XML data
+; tag - the tag that will be extracted from the XML.
+;
+; The data between the opening <TAG> and closing </TAG> is returned as a char list.
+; The data that occurs after the </TAG> is located, is returned as a string unparsed.
+; The return format for this function is (parsed_char_array . the_rest_as_string)
+(defun extractXMLTag (xml tag)
+  (let* ((xmlCharacters (coerce xml 'list)))
+    (if (equal (car xmlCharacters) #\<)
+        (if (equal (cadr xmlCharacters) #\/)
+            (if (equal (coerce (list (caddr xmlCharacters) (cadddr xmlCharacters)) 'string) tag)
+                (let* ((therest (cdr (cddddr xmlCharacters))))
+                  (if (equal nil therest)
+                      (cons nil nil)
+                      (cons nil (coerce therest 'string))))
+                (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 'string) tag)))
+                      (cons (cons (car xmlCharacters) (car rec)) (cdr rec))))
+                
+            ; Acquire the next two characters to determine the tag
+            (let* ((parsed (coerce (list (cadr xmlCharacters) (caddr xmlCharacters)) 'string)))
+              (if (equal parsed tag)
+                  (extractXMLTag (coerce (cddddr xmlCharacters) 'string) tag)
+                  (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 'string) tag)))
+                    (cons (cons (car xmlCharacters) (car rec)) (cdr rec))))))
+        
+        (if (endp xmlCharacters)
+            (cons nil nil); Nothing to see here!
+            (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 'string) tag)))
+              (cons (cons (car xmlCharacters) (car rec)) (cdr rec)))))))
+
+; (extractStockRecords xml)
+; Extracts the data encapsulated by the stock records and returns the string of those values.
+(defun extractStockRecords (xml)
+  (if (equal nil xml)
+      nil
+      (let* ((record (extractXMLTag xml "sr")))
+        (cons (coerce (car record) 'string) (extractStockRecords(cdr record))))))
+
+(defun read-data (file state)
+    (mv-let (input-text error-open state)
+            (file->string file state)
+            input-text))
