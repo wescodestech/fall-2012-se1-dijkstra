1e6b3746b6d5b0ef72cbeecdd3cb2a24c82fd2aa
tshehan6 <tshehan6@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-11-27 21:54:56 +0000

    modularized output, replaced the contents of Moutput.lisp with output.lisp, and modified Routput.lisp

--- Routput.lisp
+++ Routput.lisp
@@ -1,5 +1,9 @@
+;; The first four lines of this file were added by Dracula.
+;; They tell DrScheme that this is a Dracula Modular ACL2 program.
+;; Leave these lines unchanged so that DrScheme can properly load this file.
+#reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
 #|This line is 75 characters long.---------------------------------------|#
-#|tImpl: Stock History Analysis 
+#|tImpl: Stock History Analysis
   Routput.lisp
   Run file that runs the whole project.
 
@@ -9,14 +13,13 @@
   Date: December 6th, 2012
   Team Dijkstra
 |#
-(require "Mdriver.lisp")
-(require "Mstocks.lisp")
-(require "Mrequests.lisp")
+;(require "Mdriver.lisp")
+;(require "Mstocks.lisp")
+;(require "Mrequests.lisp")
 (require "Moutput.lisp")
-
 (link Routput
       (import)
       (export Ioutput)
-      (Mdriver Mstocks Mrequests Moutput))
-
+      (Moutput))
 (invoke Routput)
+(outputStockData (list ( list (list '(20121115 10) '(20121116 15) '(20121117 50) '(20121118 1)) (list "GOOG" "FB" "MSFT")) ( list (list '(20121112 20) '(20121113 70) '(20121114 40)) (list "AMZN" "ZNGA")))  )
