4529c3cad5b44f0aea5fd8c93325ab690dd93872
wesleyrhowell@gmail.com <wesleyrhowell@gmail.com@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-12-04 22:37:18 +0000

    Some work on the modularization. There are still issues with linking to run the driver

--- trunk/Idriver.lisp
+++ trunk/Idriver.lisp
@@ -1,3 +1,7 @@
+;; The first four lines of this file were added by Dracula.
+;; They tell DrScheme that this is a Dracula Modular ACL2 program.
+;; Leave these lines unchanged so that DrScheme can properly load this file.
+#reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
 #|This line is 75 characters long.---------------------------------------|#
 #|tImpl: Stock History Analysis 
   Idriver.lisp
@@ -7,7 +11,6 @@
 |#
 
 (interface Idriver
-  (sig isDriver (tks sr))
   (sig calcValue (tks srs total_value))
   (sig getStockValues (flattened_tree request))
   (sig mapData (stocks requests)))
