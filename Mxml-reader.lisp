7690a6e99aadab24c2a5b9e3e91be67bec6014aa
matthew.a.crist@gmail.com <matthew.a.crist@gmail.com@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-12-04 00:45:50 +0000

    Added hist.txt and short_hist.txt files to the SVN.  Also updated the Mstocks.lisp to Modular ACL2 code and deemed it complete.  Updated Mxml-reader.lisp file (non modular ACL2) that is updated to ignore whitespace.

--- Mxml-reader.lisp
+++ Mxml-reader.lisp
@@ -1,47 +1,58 @@
 (include-book "io-utilities" :dir :teachpacks)
 
 ; (extractXMLTag xml tag)
-; Extracts the contents of an XML tags (all the data that is contained between
-; the tags)
+; Extracts the contents of an XML tags (all the data that is contained 
+; between the tags)
+;
 ; xml - the string that contains the RAW XML data
 ; tag - the tag that will be extracted from the XML.
 ;
-; The data between the opening <TAG> and closing </TAG> is returned as a char list.
-; The data that occurs after the </TAG> is located, is returned as a string unparsed.
-; The return format for this function is (parsed_char_array . the_rest_as_string)
+; The data between the opening <TAG> and closing </TAG> is returned as a 
+; char list.  The data that occurs after the </TAG> is located, is 
+; returned as a string unparsed. The return format for this function is 
+; (parsed_char_array . the_rest_as_string)
 (defun extractXMLTag (xml tag)
+  ; Covert the XML string into a character list
   (let* ((xmlCharacters (coerce xml 'list)))
+    ; See if we are working with a tag
     (if (equal (car xmlCharacters) #\<)
+        ; Is it a closing tag?
         (if (equal (cadr xmlCharacters) #\/)
-            (if (equal (coerce (list (caddr xmlCharacters) (cadddr xmlCharacters)) 'string) tag)
+            ; Is it the closing tag of our tag we are looking for?
+            (if (equal (coerce (list (caddr xmlCharacters) 
+                                     (cadddr xmlCharacters)) 'string) tag)
+                ; Ingore the rest of the tag
                 (let* ((therest (cdr (cddddr xmlCharacters))))
+                  ; Done processing XML!
                   (if (equal nil therest)
+                      ; No more XML to process, return nil
                       (cons nil nil)
+                      ; More XML let, return it as a string
                       (cons nil (coerce therest 'string))))
-                (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 'string) tag)))
-                      (cons (cons (car xmlCharacters) (car rec)) (cdr rec))))
+                ; Keep processing the record
+                (let* ((rec (extractXMLTag 
+                             (coerce (cdr xmlCharacters) 'string) tag)))
+                  ; Add to the record char array cons the rest to process
+                      (cons (cons (car xmlCharacters) (car rec)) 
+                            (cdr rec))))
                 
             ; Acquire the next two characters to determine the tag
-            (let* ((parsed (coerce (list (cadr xmlCharacters) (caddr xmlCharacters)) 'string)))
+            (let* ((parsed (coerce (list (cadr xmlCharacters) 
+                                         (caddr xmlCharacters)) 'string)))
+              ; We found our opening tag!
               (if (equal parsed tag)
-                  (extractXMLTag (coerce (cddddr xmlCharacters) 'string) tag)
-                  (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 'string) tag)))
-                    (cons (cons (car xmlCharacters) (car rec)) (cdr rec))))))
+                  (extractXMLTag (coerce (cddddr xmlCharacters) 'string) 
+                                 tag)
+                  (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 
+                                                     'string) tag)))
+                    (cons (cons (car xmlCharacters) (car rec)) 
+                          (cdr rec))))))
         
         (if (endp xmlCharacters)
             (cons nil nil); Nothing to see here!
             (let* ((rec (extractXMLTag (coerce (cdr xmlCharacters) 'string) tag)))
-              (cons (cons (car xmlCharacters) (car rec)) (cdr rec)))))))
-
-; (extractStockRecords xml)
-; Extracts the data encapsulated by the stock records and returns the string of those values.
-(defun extractStockRecords (xml)
-  (if (equal nil xml)
-      nil
-      (let* ((record (extractXMLTag xml "sr")))
-        (cons (coerce (car record) 'string) (extractStockRecords(cdr record))))))
-
-(defun read-data (file state)
-    (mv-let (input-text error-open state)
-            (file->string file state)
-            input-text))
+              (if (or (equal (car xmlCharacters) #\Newline)
+                    (equal (car xmlCharacters) #\Tab))
+                  ; Ignore the tab/newline
+                  (cons (car rec) (cdr rec))
+                  (cons (cons (car xmlCharacters) (car rec)) (cdr rec)))))))) 
