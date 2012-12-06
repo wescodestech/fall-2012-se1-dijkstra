5b31da4f66c5b69016fce9d59bff075970c9761c
matthew.a.crist@gmail.com <matthew.a.crist@gmail.com@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-12-06 20:04:41 +0000

    Working on randomized testing for Tdriver.lisp.

--- Tdriver.lisp
+++ Tdriver.lisp
@@ -1,3 +1,7 @@
+;; The first four lines of this file were added by Dracula.
+;; They tell DrScheme that this is a Dracula Modular ACL2 program.
+;; Leave these lines unchanged so that DrScheme can properly load this file.
+#reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
 
 (require "Mlex-ops.lisp")
 (require "Mxml-reader.lisp")
@@ -34,12 +38,14 @@
      srs :value (list (cons "AA" 12.12) (cons "YHOO" 14.12)))
     (rationalp (calcValue tks srs 0)))
   
-  
+  (defproperty predicateTest
+    (tk :value (random-string)
+        tks :value (random-list-of (random-string)))
+    (isPresent (cons tks tk) tk))
   
   
   )
 (link RTdriver
-
       (Mlex-ops Mxml-reader Mstocks Mrequests MOutput Mdriver Tdriver))
 (invoke RTdriver)
 
