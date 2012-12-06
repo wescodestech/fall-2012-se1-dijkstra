84f08cb0af478a4966437aa3b5f605ced7e3c496
matthew.a.crist@gmail.com <matthew.a.crist@gmail.com@80ca3a89-09f6-ce9a-c812-4045315a434d>
2012-12-06 21:58:52 +0000

    Updated check expects and added a few property based tests.

--- Tdriver.lisp
+++ Tdriver.lisp
@@ -2,7 +2,6 @@
 ;; They tell DrScheme that this is a Dracula Modular ACL2 program.
 ;; Leave these lines unchanged so that DrScheme can properly load this file.
 #reader(planet "reader.rkt" ("cce" "dracula.plt") "modular" "lang")
-
 (require "Mlex-ops.lisp")
 (require "Mxml-reader.lisp")
 (require "Mstocks.lisp")
@@ -21,6 +20,7 @@
   
   (include-book "testing" :dir :teachpacks)
   (include-book "doublecheck" :dir :teachpacks)
+  (include-book "avl-rational-keys" :dir :teachpacks)
   
   (check-expect (isPresent '("AA" "AAPL") "AA") t)
   (check-expect (isPresent '("AA" "AAPL" "GOOG") "YHOO") nil)
@@ -32,19 +32,41 @@
                            (list (cons "YHOO" 12.12) (cons "YHOO" 14.12))
                            0) 0)
   
+  ; getStockValues check-expects
+  ; Searching an empty tree
+  (check-expect (getStockValues (avl-flatten (empty-tree)) 
+                                '((20121203 20121214 '("AA" "AAPL")))) nil)
+  ; Searching for a value that is not present
+  (check-expect (getStockValues (avl-flatten 
+                                 (avl-insert (empty-tree) 
+                                             20121203 '(("AA" 12.32))))
+                                '(20121023 20121024 ("AA"))) nil)
+  ; Searching for nothing
+  (check-expect (getStockValues (avl-flatten 
+                                 (avl-insert (empty-tree) 
+                                             20121203 '()))
+                                '(20121023 20121024 ("AA"))) nil)
   
-  (defproperty calcValueratP
-    (tks :value (list "AA" "AAPL" "GOOG") 
-     srs :value (list (cons "AA" 12.12) (cons "YHOO" 14.12)))
-    (rationalp (calcValue tks srs 0)))
-  
-  (defproperty predicateTest
+  ; isPresent-tst
+  ; Checks the validity of the isPresent function.  Ensures that a token
+  ; is present in the list that is generated.  If that value is cons'd
+  ; into the list, it should be present in the list.
+  (defproperty isPresent-tst
     (tk :value (random-string)
         tks :value (random-list-of (random-string)))
-    (isPresent (cons tks tk) tk))
+    (isPresent (cons tk tks) tk))
   
-  
-  )
+  ; getStockValues-tst
+  ; Checks that if a token exists in a set of token that is fed to the AVL
+  ; tree, then the getStockValues-tst will not return nil.
+  (defproperty getStockValues-tst
+    (tk :value (random-string)
+        tks :value (random-list-of (random-string))
+        date :value (random-rational))
+    (implies (isPresent tks tk)
+             (getStockValues (avl-flatten (avl-insert (empty-tree)
+                                                      date (list tks)))
+                             (list date date tks)))))
 (link RTdriver
       (Mlex-ops Mxml-reader Mstocks Mrequests MOutput Mdriver Tdriver))
 (invoke RTdriver)
